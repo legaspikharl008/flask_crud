345244db57d6b5bd4623c384712e96c300fb303a
legaspikharl008 <145105420+legaspikharl008@users.noreply.github.com>
2025-10-07 15:53:19 +0900

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ class Config:
     SECRET_KEY = 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+app.config["UPLOAD_FOLDER"] = os.path.join(app.root_path, "static/uploads")
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+
+
